e80f1657f1d674c45e829230faab681b4004b3ca
Sergey Zubkov <ksevelyar@protonmail.com>
2024-05-27 23:00:09 +0300

    update

--- flake.nix
+++ flake.nix
@@ -1,27 +1,30 @@
 {
+  description = "A devShell example";
+
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
-    flake-utils.url = github:numtide/flake-utils;
-    rust-overlay.url = github:oxalica/rust-overlay;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
       system: let
+        overlays = [(import rust-overlay)];
         pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ rust-overlay.overlays.default ];
+          inherit system overlays;
         };
       in
-        {
-          devShell = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-              (
-                rust-bin.stable.latest.default.override {
-                  targets = [];
-                }
-              )
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = [
+              openssl
               pkg-config
+              rust-bin.stable.latest.default
             ];
           };
         }
